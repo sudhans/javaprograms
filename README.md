fcb3610d62aeb5ce82e014c74fe3c6916bb86238
msd <sudhans@gmail.com>
2021-08-01 08:45:03 +0530

    Added ReadMe and First Java File

---
+++ README.md
@@ -0,0 +1,1 @@
+Java Coding Problems solved using JDK8 and above.
---
+++ src/com/msd/coding/puzzles/DuplicateCharacterInStrings.java
@@ -0,0 +1,31 @@
+package com.msd.coding.puzzles;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Find if the given string has duplicate characters
+ */
+public class DuplicateCharacterInStrings {
+    public static void main(String[] args) {
+        String str1 = "KingOfChennai";
+        String str2 = "Kingdom";
+        System.out.println("Has Duplicate Chars :: " + hasDuplicateChars(str1));
+        System.out.println("Has Duplicate Chars :: " + hasDuplicateChars(str2));
+
+        System.out.println("Has Duplicate Chars by distinct:: " + hasDuplicateCharsByDistinct(str1));
+        System.out.println("Has Duplicate Chars by distinct :: " + hasDuplicateCharsByDistinct(str2));
+    }
+
+    private static boolean hasDuplicateChars(String input) {
+        Map<Character, Long> characterMap = input.chars().mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
+        return characterMap.values().stream().filter(count -> count > 1).count() > 1;
+    }
+
+    private static boolean hasDuplicateCharsByDistinct(String input) {
+        return input.length() > input.chars().mapToObj(c -> (char) c).distinct().count();
+    }
+}
